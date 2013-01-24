c859c2d82c222853156470e34a7f7b25f9e2c536
DrNicolaRodgers <nicola.rodgers@medschool.ox.ac.uk>
2013-01-24 23:18:18 +0000

    Update javascripts/main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,149 @@
-$(function(){
-    
-	$("#output").text("Hello !");
+$('document').ready(function() {
+var player = window.prompt("Rock, paper, scissors, lizard or spock?");
+
+if (player !== null){
+    player = player.toLowerCase();
+}
+
+var choices = ["rock","paper","scissors", "lizard", "spock"];
+var computer = choices[Math.floor(Math.random()*5)];
+
+var win = "Your "+player+" beats "+computer+". You win.";
+var lose = "Your "+player+" loses to "+computer+". You lose.";
+var draw = "A draw: "+player+" on "+computer+".";
+
+if(player === "rock"){
+    switch(computer){
+        
+      case "scissors":
+      result = win;
+      break;
+
+      case "paper":
+      result = lose;
+      break;
+
+      case "rock":
+      result = draw;
+      break;
+      
+      case "lizard":
+      result = win;
+      break;
+
+      case "spock":
+      result = lose;
+      break;
+    }
+}
+
+else if(player === "paper"){
+
+    switch(computer){
+        
+      case "scissors":
+      result = lose;
+      break;
+
+      case "paper":
+      result = draw;
+      break;
+
+      case "rock":
+      result = win;
+      break;
+      
+      case "lizard":
+      result = lose;
+      break;
+
+      case "spock":
+      result = win;
+      break;
+    }
+}
+else if(player === "scissors"){
+   
+    switch(computer){
+        
+      case "scissors":
+      result = draw;
+      break;
+
+      case "paper":
+      result = win;
+      break;
+
+      case "rock":
+      result = lose;
+      break;
+      
+      case "lizard":
+      result = win;
+      break;
+
+      case "spock":
+      result = lose;
+      break;
+    }
+}
+
+else if(player === "lizard"){
+   
+    switch(computer){
+        
+      case "scissors":
+      result = lose;
+      break;
+
+      case "paper":
+      result = win;
+      break;
+
+      case "rock":
+      result = lose;
+      break;
+      
+      case "lizard":
+      result = draw;
+      break;
+
+      case "spock":
+      result = win;
+      break;
+    }
+}
+
+else if(player === "spock"){
+   
+    switch(computer){
+        
+      case "scissors":
+      result = win;
+      break;
+
+      case "paper":
+      result = lose;
+      break;
+
+      case "rock":
+      result = win;
+      break;
+      
+      case "lizard":
+      result = lose;
+      break;
+
+      case "spock":
+      result = draw;
+      break;
+    }
+}
+else if(player === null){
+  result = "Bye!";
+}
+else{
+  result = "I said rock, paper, scissors, lizard or spock!"
+}
+$("#output").text("Help!!"+result+);
 });
