90a9a4791e9b73621529d4d26315ab828415ea7a
DrNicolaRodgers <nicola.rodgers@medschool.ox.ac.uk>
2013-01-24 05:23:53 -0800

    Create rps.js

---
+++ rps.js
@@ -0,0 +1,70 @@
+var player = prompt("Rock, paper or scissors?)
+if (player !== null){
+  player = player.toLowerCase();
+}
+
+var choices = ["rock","paper","scissors"];
+var computer = choices[Math.floor(Math.random()*3)];
+
+var win = "Your "+player+" beats "+computer+". You win.";
+var lose = "Your "+player+" loses to "+computer+". You lose.";
+var draw = "A draw: "+player+" on "+computer+".";
+
+if(player === "rock"){
+    switch(computer){
+        
+      case "scissors":
+      result = win;
+      break;
+
+      case "paper":
+      result = lose;
+      break;
+
+      case "rock":
+      result = draw;
+      break;
+    }
+}
+
+else if(player === "paper"){
+
+    switch(computer){
+        
+      case "scissors":
+      result = lose;
+      break;
+
+      case "paper":
+      result = draw;
+      break;
+
+      case "rock":
+      result = win;
+      break;
+    }
+}
+else if(player === "scissors"){
+   
+    switch(computer){
+        
+      case "scissors":
+      result = draw;
+      break;
+
+      case "paper":
+      result = win;
+      break;
+
+      case "rock":
+      result = lose;
+      break;
+    }
+}
+else if(player === null){
+  console.log ("Bye!");
+}
+else{
+	console.log("I said rock, paper, or scissors!")
+}
+console.log(result);
